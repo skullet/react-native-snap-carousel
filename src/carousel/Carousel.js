765e80f85675d10e30a2cd51ec0b6eefb7cfc86c
Nathan Peel <nate@appalytical.com>
2022-05-06 14:31:32 -0600

    make the forked carousel use scrollview and flatlist from rn gesture handler so they work in gorham's bottomsheet

--- src/carousel/Carousel.js
+++ src/carousel/Carousel.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { Animated, Easing, FlatList, I18nManager, Platform, ScrollView, View, ViewPropTypes } from 'react-native';
+import { Animated, Easing, I18nManager, Platform, View, ViewPropTypes } from 'react-native';
+import {
+    ScrollView,
+    FlatList
+} from 'react-native-gesture-handler';
 import PropTypes from 'prop-types';
 import shallowCompare from 'react-addons-shallow-compare';
 import {
